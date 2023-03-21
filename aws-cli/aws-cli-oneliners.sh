829effe5944f64b4a81ab24753b659803420c505
Nikita Zabelin <64374751+hebrewbug@users.noreply.github.com>
2023-03-22 00:02:29 +0200

    Update aws-cli-oneliners.sh

--- aws-cli/aws-cli-oneliners.sh
+++ aws-cli/aws-cli-oneliners.sh
@@ -1,3 +1,5 @@
 cat $HOME/.aws/credentials #show placement of AWS credentials
 
 aws configure #configure aws cli
+
+cat /etc/system-release #You can view your version of Amazon Linux using the following command.
