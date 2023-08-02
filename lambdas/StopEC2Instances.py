886c5477f42bd97ad14cf98eb9209498c70101c3
Nikita Zabelin <64374751+nikitazabelin@users.noreply.github.com>
2023-08-02 12:32:04 +0300

    Upgrade and fix StopEC2Instances.py

--- lambdas/StopEC2Instances.py
+++ lambdas/StopEC2Instances.py
@@ -4,10 +4,10 @@ def lambda_handler(event, context):
 
   ec2_client = boto3.client('ec2')
   regions = [region['RegionName']
-             for region in ec2_client.describe_regions()['Regions]]
+             for region in ec2_client.describe_regions()['Regions']]
 
   for region in regions:
-      ec2 = boto3.resources('ec2', region_name=region)
+      ec2 = boto3.resource('ec2', region_name=region)
 
       print("Region:", region)
 
